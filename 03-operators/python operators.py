b9fbce956bf72cd6e0b81cb6189151df6a65844f
unknown <taghizadeh.mhmd@gmail.com>
2022-02-15 23:13:20 +0330

    03-operators

---
+++ 03-operators/python operators.py
@@ -0,0 +1,9 @@
+# Python divides the operators in the following groups:
+
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
