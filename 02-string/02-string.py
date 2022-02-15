08800bd45280c9df18799f691ba981f66c931a62
unknown <taghizadeh.mhmd@gmail.com>
2022-02-15 23:13:03 +0330

    02-string

---
+++ 02-string/02-string.py
@@ -0,0 +1,125 @@
+### define
+message = 'Python Programming Language'
+print(message)
+
+
+### multiple lines
+msg = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(msg)
+
+
+### string array
+print(msg[1])
+
+# loop string
+for ch in msg:
+    print(ch)
+
+
+# check string
+if "Programming" not in message:
+    print("True")
+else:
+    print("False")
+
+
+### slicing
+print(msg[2:5])
+print(msg[2:])
+print(msg[:5])
+print(msg[:-1])
+print(msg[-5:-2])
+
+
+### len
+print(len(msg))
+
+
+### string modify methods
+print(msg.lower())
+print(msg.strip())
+print(msg.upper())
+print(msg.replace("H", "J"))
+print(msg.split(","))
+
+
+### concat
+print(msg + "end")
+
+
+### format
+name = 'mohammad'
+age = 22
+msg = "man {} hastam va {} am shude.. "
+print(msg.format(name, age))
+
+
+# F-string
+print(f"name {name}")
+
+
+# Operators
+print(msg*2)
+
+
+# \ Escape Character
+# \'	Single Quote	
+# \\	Backslash	
+# \n	New Line	
+# \r	Carriage Return	
+# \t	Tab	
+# \b	Backspace	
+# \f	Form Feed	
+# \ooo	Octal value	
+# \xhh	Hex value
+print(message+"\n")
+
+
+# String methods 
+# capitalize()	Converts the first character to upper case
+# casefold()	Converts string into lower case
+# center()	Returns a centered string
+# count()	Returns the number of times a specified value occurs in a string
+# encode()	Returns an encoded version of the string
+# endswith()	Returns true if the string ends with the specified value
+# expandtabs()	Sets the tab size of the string
+# find()	Searches the string for a specified value and returns the position of where it was found
+# format()	Formats specified values in a string
+# format_map()	Formats specified values in a string
+# index()	Searches the string for a specified value and returns the position of where it was found
+# isalnum()	Returns True if all characters in the string are alphanumeric
+# isalpha()	Returns True if all characters in the string are in the alphabet
+# isdecimal()	Returns True if all characters in the string are decimals
+# isdigit()	Returns True if all characters in the string are digits
+# isidentifier()	Returns True if the string is an identifier
+# islower()	Returns True if all characters in the string are lower case
+# isnumeric()	Returns True if all characters in the string are numeric
+# isprintable()	Returns True if all characters in the string are printable
+# isspace()	Returns True if all characters in the string are whitespaces
+# istitle()	Returns True if the string follows the rules of a title
+# isupper()	Returns True if all characters in the string are upper case
+# join()	Joins the elements of an iterable to the end of the string
+# ljust()	Returns a left justified version of the string
+# lower()	Converts a string into lower case
+# lstrip()	Returns a left trim version of the string
+# maketrans()	Returns a translation table to be used in translations
+# partition()	Returns a tuple where the string is parted into three parts
+# replace()	Returns a string where a specified value is replaced with a specified value
+# rfind()	Searches the string for a specified value and returns the last position of where it was found
+# rindex()	Searches the string for a specified value and returns the last position of where it was found
+# rjust()	Returns a right justified version of the string
+# rpartition()	Returns a tuple where the string is parted into three parts
+# rsplit()	Splits the string at the specified separator, and returns a list
+# rstrip()	Returns a right trim version of the string
+# split()	Splits the string at the specified separator, and returns a list
+# splitlines()	Splits the string at line breaks and returns a list
+# startswith()	Returns true if the string starts with the specified value
+# strip()	Returns a trimmed version of the string
+# swapcase()	Swaps cases, lower case becomes upper case and vice versa
+# title()	Converts the first character of each word to upper case
+# translate()	Returns a translated string
+# upper()	Converts a string into upper case
+# zfill()	Fills the string with a specified number of 0 values at the beginning
