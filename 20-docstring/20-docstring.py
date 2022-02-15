bcf484719d601ba9eb8ad0732b285481b42f4bb5
unknown <taghizadeh.mhmd@gmail.com>
2022-02-15 23:19:28 +0330

    20-docstring

---
+++ 20-docstring/20-docstring.py
@@ -0,0 +1,11 @@
+def my_function():
+    '''Demonstrates triple double quotes
+    docstrings and does nothing really.
+    thats for documentations
+    '''
+   
+    return None
+  
+
+print(my_function.__doc__)
+help(my_function)
