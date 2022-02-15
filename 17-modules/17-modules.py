b0fe4c04990058c7095039aa30758e44ac137eab
unknown <taghizadeh.mhmd@gmail.com>
2022-02-15 23:18:45 +0330

    17-modules

---
+++ 17-modules/17-modules.py
@@ -0,0 +1,12 @@
+# from my_module import print_welcome
+from black import mypyc_attr
+import my_module 
+
+my_module.print_welcome('mohammad')
+print(my_module.person1)
+
+
+# dir()
+print(dir(my_module))
+import math
+print(dir(math))
---
+++ 17-modules/my_module.py
@@ -0,0 +1,12 @@
+import datetime
+
+def print_welcome(name):
+    print('welcome to python %s ' %name)
+    print(datetime.datetime.now())
+
+    
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
