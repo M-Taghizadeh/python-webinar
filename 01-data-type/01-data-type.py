90976c88628ae6fc32fae0524176f8141261d030
unknown <taghizadeh.mhmd@gmail.com>
2022-02-15 23:12:44 +0330

    01-data-type

---
+++ 01-data-type/01-data-type.py
@@ -0,0 +1,31 @@
+name = "Mohammad Taghizadeh" # String
+
+age = 23 # Integer
+
+avg = 45.46 # Float
+
+products = {
+    "name": "Car",
+    "price": 22000000,
+    "speed": "200km/h",
+}
+
+my_set = {"apple", "car", "mobile", "computer"}
+
+my_iter = range(10)
+
+flag = True 
+
+my_list = [name, "test", my_set, products]
+
+my_tuple = ("mobile", "laptop", "pc")
+
+# type function
+print(type(products))
+
+# type casting
+str_product = str(products)
+print(str_product, type(str_product))
+
+number = int(input("Enter number: "))
+print(number, type(number))
